0e141f7cf290b3f1b87cba3f3b86db1f7da78b1d
ivan <evolved.python@gmail.com>
2025-02-10 17:58:58 +0200

    loopsRefsAuto exercise - solved

--- exercises/loopsRefsAuto/loopsRefsAuto.cpp
+++ exercises/loopsRefsAuto/loopsRefsAuto.cpp
@@ -20,6 +20,11 @@ int main() {
    // Write a for loop that initialises each struct's resultA and resultB with ascending integers.
    // Verify the output of the program before and after you do this.
 
+   for (int i = 0; i < 10; ++i) {
+      collection[i].resultA = i * 2;     // arbitrary initialization
+      collection[i].resultB = i * 2 + 1; // or any other ascending logic
+  }
+
 
    // Task 2:
    // We use a range-based for loop to analyse the array of structs.
@@ -28,7 +33,8 @@ int main() {
    // Hint: Fix the type declaration "auto" in the loop head.
    int resultA = 0;
    int resultB = 0;
-   for (auto item : collection) {
+   // Here, we only read from each element, so we use const &.
+   for (auto const &item : collection) {
       resultA += item.resultA;
       resultB += item.resultB;
    }
