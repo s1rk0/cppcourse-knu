a743697c7f67e5f611a700e03f25db4449476c4f
ivan <evolved.python@gmail.com>
2025-06-10 16:57:30 +0300

    stl exercise solved

--- exercises/stl/randomize.cpp
+++ exercises/stl/randomize.cpp
@@ -11,15 +11,16 @@ void compute(int len, T initial, T step) {
     std::vector<T> v(len+1), diffs(len+1);
 
     // fill and randomize v
-    std::generate(, , [](...) { ...; });
-    std::shuffle(..., std::default_random_engine{});
+    T current = initial;
+    std::generate(v.begin(), v.end(), [&current, step]() { T val = current; current += step; return val; });
+    std::shuffle(v.begin(), v.end(), std::default_random_engine{});
 
     // compute differences
-    std::adjacent_difference(...);
+    std::adjacent_difference(v.begin(), v.end(), diffs.begin());
 
     // compute standard deviation of all differences
-    const T sum = std::reduce(...);
-    const T sumsq = std::accumulate(..., [](...) { ...; });
+    const T sum = std::reduce(diffs.begin(), diffs.end());
+    const T sumsq = std::accumulate(diffs.begin(), diffs.end(), T{}, [](const T& acc, const T& x) { return acc + x * x; });
     const T mean = sum/len;
     const T variance = sumsq/len - mean*mean;
 
@@ -29,6 +30,9 @@ void compute(int len, T initial, T step) {
 }
 
 int main() {
+    std::cout << "Double:\n";
     compute(1000, 0.0, 7.0);
     // call compute here with Complex
+    std::cout << "\nComplex:\n";
+    compute(1000, Complex(0.0f, 0.0f), Complex(7.0f, 7.0f));
 }
